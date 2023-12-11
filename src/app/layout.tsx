42aeddb536b9634c51694e6206dcf76deb2aeaad
eder3232 <eldebar.dk@gmail.com>
2023-12-10 22:55:00 -0500

    tratando de usar radixui con daisyui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <html lang="en">
+      <html lang="en" data-theme="light">
         <body className={inter.className}>{children}</body>
       </html>
     </ClerkProvider>
